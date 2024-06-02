ced977bf3d436826099ca6d131375a7bb2793612
abhilash9099 <Abhilashreddychemarla@gmail.com>
2024-06-02 00:41:34 -0500

    tickets please api using laravel 11

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,13 +3,18 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Http\Filters\V1\QueryFilter;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -20,6 +25,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'is_manager'
     ];
 
     /**
@@ -33,15 +39,21 @@ class User extends Authenticatable
     ];
 
     /**
-     * Get the attributes that should be cast.
+     * The attributes that should be cast.
      *
-     * @return array<string, string>
+     * @var array<string, string>
      */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+        'is_manager' => 'boolean'
+    ];
+
+    public function tickets() : HasMany {
+        return $this->hasMany(Ticket::class);
+    }
+
+    public function scopeFilter(Builder $builder, QueryFilter $filters) {
+        return $filters->apply($builder);
     }
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,18 +1,84 @@
 <?php
 
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
+        then: function () {
+            Route::middleware('api')
+                ->prefix('api/v1')
+                ->group(base_path('routes/api_v1.php'));
+        }
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (ValidationException $exception) {
+            foreach ($exception->errors() as $key => $value) {
+                foreach ($value as $message) {
+                    $errors[] = [
+                        'status' => 422,
+                        'message' => $message,
+                        'source' => $key
+                    ];
+                }
+            }
+            return response()->json([
+                'errors' => $errors
+            ]);
+        });
+
+        $exceptions->render(function (ModelNotFoundException $exception) {
+            $errors = [
+                [
+                    'status' => 404,
+                    'message' => 'The resource cannot be found.',
+                    'source' => $exception->getModel()
+                ]
+            ];
+            return response()->json([
+                'errors' => $errors
+            ]);
+        });
+
+        $exceptions->render(function (AuthenticationException $exception) {
+            $errors = [
+                [
+                    'status' => 404,
+                    'message' => 'Unauthenticated',
+                    'source' => ''
+                ]
+            ];
+            return response()->json([
+                'errors' => $errors
+            ]);
+        });
+
+        $exceptions->render(function (Throwable $exception) {
+            $className = get_class($exception);
+            $index = strrpos($className, '\\');
+
+            $errors = [
+                [
+                    'type' => substr($className, $index + 1),
+                    'status' => 0,
+                    'message' => $exception->getMessage(),
+                    'source' => 'Line: ' . $exception->getLine() . ': ' . $exception->getFile()
+                ]
+            ];
+            return response()->json([
+                'errors' => $errors
+            ]);
+        });
     })->create();
